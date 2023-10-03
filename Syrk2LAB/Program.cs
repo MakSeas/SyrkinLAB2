732539d44ba82e78ef7847aa676c2ef19ed7c9dc
MakSeas <koshmus2003@yandex.ru>
2023-10-03 16:07:51 +0700

    Добавьте файлы проекта.

---
+++ Syrk2LAB/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syrk2LAB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выбор пути:\n " +
+                "0 - Ввести путь вручную\n" +
+                "1 - Продолжить путь с C:\\Users\\koshm\\OneDrive\\ \n" +
+                "2 - Сохранить на рабочий стол\n");
+
+            int act=int.Parse(Console.ReadLine());
+
+            string path="";
+
+            switch (act) 
+            {
+                case 0:
+                    {
+                        path = Console.ReadLine();
+                    }break;
+                case 1:
+                    {
+                        Console.Write("C:\\Users\\koshm\\OneDrive\\");
+                        path= "C:\\Users\\koshm\\OneDrive\\"+Console.ReadLine();
+                    }break;
+                case 2:
+                    {
+                        path = "C:\\Users\\koshm\\OneDrive\\Desktop\\";
+                    }
+                    break;
+                default: break;
+            }
+
+            Console.WriteLine($"Выбран путь {path}\n");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            Console.Write("Имя txt файла: ");
+            string name = Console.ReadLine();
+
+            path += $"{name}.txt";
+
+            Console.WriteLine("Выбор кодировки:\n " +
+                "0 - UTF-8\n" +
+                "1 - Win1251\n" +
+                "2 - DOC 866\n");
+
+            act = int.Parse(Console.ReadLine());
+
+            TextWriter textWriter=null;
+
+
+
+            switch (act)
+            {
+                case 0:
+                    {
+                        textWriter = SetEncoding(path, Encoding.UTF8);
+                    }
+                    break;
+                case 1:
+                    {
+                        textWriter = SetEncoding(path, Encoding.UTF8);
+                        // textWriter = SetEncoding(path, Encoding.Win1251);
+                    }
+                    break;
+                case 2:
+                    {
+                        textWriter = SetEncoding(path, Encoding.UTF8);
+                        // textWriter = SetEncoding(path, Encoding.DOC866);
+                    }
+                    break;
+                default: break;
+            }
+
+            
+
+            Console.Write("Текст, который будет отображен в txt файле: ");
+            string text = Console.ReadLine();
+
+            textWriter.WriteLine(text);
+
+            textWriter.Close();
+        }
+
+        static TextWriter SetEncoding(string path, Encoding encoding)
+        {
+            return new StreamWriter(path, true, encoding); ;
+        }
+    }
+}
---
+++ Syrk2LAB/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Syrk2LAB")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Syrk2LAB")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ee3df7ec-31dd-4e2e-9abb-8438d91c2935")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
